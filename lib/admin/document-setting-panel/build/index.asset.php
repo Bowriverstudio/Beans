4d8a2632e8ffc51e757e5917ef70793893621437
Maurice Tadros <maurice.tadros@gmail.com>
2019-11-12 17:14:53 -0700

    Added icon for beans

--- lib/admin/document-setting-panel/build/index.asset.php
+++ lib/admin/document-setting-panel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'f712a25eb679ebe8752cae433c0f676b');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'fb21b896812d16b62839bc894fc5284c');
