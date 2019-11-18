2e276a9b1d63089de3c0e7870641ed59b1d5612b
Maurice Tadros <maurice.tadros@gmail.com>
2019-11-18 15:57:17 -0700

    Added command wp beans constants

--- lib/api/wpcli/class-beans-wpcli.php
+++ lib/api/wpcli/class-beans-wpcli.php
@@ -15,7 +15,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	{
 
 		/**
-		 * Show all defined Beans Constants
+		 * Show commonly used Beans Constants
 		 *
 		 * ## Constants
 		 *
@@ -32,10 +32,15 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 */
 		public function constants()
 		{
-
-			WP_CLI::log("TEST");
-
+			WP_CLI::log("BEANS_VERSION: " .BEANS_VERSION);
+			WP_CLI::log("BEANS_THEME_PATH: " .BEANS_THEME_PATH);
+			WP_CLI::log("BEANS_THEME_URL: " .BEANS_THEME_URL);
+			WP_CLI::log("CHILD_PATH: " .CHILD_PATH);
+			WP_CLI::log("CHILD_URL: " .CHILD_URL);
+			WP_CLI::log("BEANS_FRONTEND_FRAMEWORK: " .BEANS_FRONTEND_FRAMEWORK);
 		}
+
+
 	}
 
 	WP_CLI::add_command( 'beans', 'Beans_WPCLI' );
