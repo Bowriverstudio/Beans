91bcc07f9079c1e7af2ac8bc40a0a54ba300a8df
Maurice Tadros <maurice.tadros@gmail.com>
2019-12-12 17:02:54 -0700

    Added Full Page Template

---
+++ page-templates/landing.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Beans.
+ *
+ * This file adds the landing page template to the Beans Theme.
+ *
+ * This is only available - todo condition.
+ *
+ * Template Name: Landing
+ * Template Post Type: post, page
+ *
+ */
+
+
+beans_remove_action( 'beans_header_partial_template' );
+beans_remove_action( 'beans_breadcrumb' );
+beans_remove_action( 'beans_post_title' );
+beans_remove_action( 'beans_footer_partial_template' );
+beans_remove_action( 'beans_post_navigation' );
+beans_remove_action( 'beans_post_meta_categories' );
+
+
+beans_load_document();
