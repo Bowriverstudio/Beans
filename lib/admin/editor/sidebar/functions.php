d125deb624e3bcd716bc6a6b3b3b541f6be385f8
Maurice Tadros <maurice.tadros@gmail.com>
2019-12-17 09:31:53 -0700

    Conditionally add Beans sidebar if post type supports it

--- lib/admin/editor/sidebar/functions.php
+++ lib/admin/editor/sidebar/functions.php
@@ -8,8 +8,17 @@ namespace Beans\Admin\Document_Settings_Panel;
 add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_assets');
 /**
  * Enqueue the resources needed for Beans Sidebar.
+ *
  */
 function enqueue_assets(){
+
+	// Only load these assets if the post type supports it.
+	$current_screen = get_current_screen();
+	$post_type      = $current_screen->post_type ?: '';
+
+	if ( ! post_type_supports( $post_type, 'custom-fields' ) ) {
+		return [];
+	}
 
 	$asset_file = include(BEANS_ADMIN_PATH . 'editor/sidebar/build/index.asset.php');
 	$build_url = BEANS_URL . 'admin/editor/sidebar/build/';
@@ -29,5 +38,4 @@ function enqueue_assets(){
 
 	);
 }
-
 
